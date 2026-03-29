7def1b6dd0f8cc5d59175fd12858b16a5db74a8c
MarcoHoKongMa <marcohokong.ma@ontariotechu.net>
2026-03-29 19:03:39 -0400

    feat: added crypto buy/sell sum

--- get-return.py
+++ get-return.py
@@ -7,8 +7,17 @@ if len(sys.argv) < 2:
 df = pd.read_csv(sys.argv[1])
 
 # Filter Transaction Type
-reward = df[df['Type'] == "Reward"]
+buy = df[df['Type'] == 'Buy']
+btc_buy = buy[buy['Asset Credited'] == 'BTC']
+eth_buy = buy[buy['Asset Credited'] == 'ETH']
+sell = df[df['Type'] == 'Sell']
+btc_sell = sell[sell['Asset Debited'] == 'BTC']
+eth_sell = sell[sell['Asset Debited'] == 'ETH']
+reward = df[df['Type'] == 'Reward']
 
 # Calculate Gain/Loss
+btc_buy_sum = round(btc_buy['Book Cost'].sum(), 2)
+eth_buy_sum = round(eth_buy['Book Cost'].sum(), 2)
+btc_sell_sum = round(btc_sell['Book Cost'].sum(), 2)
+eth_sell_sum = round(eth_sell['Book Cost'].sum(), 2)
 reward_sum = round(reward['Book Cost'].sum(), 2)
-print(reward_sum)
